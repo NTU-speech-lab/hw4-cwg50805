dab9d2c9fdf1658246e1292bbf11851e7a4c571a
cwg50805 <cwgcwgcwg12345@gmail.com>
2020-04-30 19:20:36 +0800

    Add semi-supervised

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ from torch import nn
 from gensim.models import word2vec
 from sklearn.model_selection import train_test_split
 
+
 import w2v 
 from utils import load_training_data, load_testing_data, evaluation
 from preprocess import Preprocess
 from data import TwitterDataset
 from model import LSTM_Net
 from train import training
-from test import  testing
+from test import  testing, psuedo_testing
 
 path_prefix = './'
 os.environ["CUDA_VISIBLE_DEVICES"] = sys.argv[1]
@@ -41,40 +42,72 @@ model_dir = path_prefix # model directory for checkpoint model
 
 print("loading data ...") # 把 'training_label.txt' 跟 'training_nolabel.txt' 讀進來
 train_x, y = load_training_data(train_with_label)
-print(train_x)
+print(type(train_x))
 train_x_no_label = load_training_data(train_no_label)
 
-# 對 input 跟 labels 做預處理
-preprocess = Preprocess(train_x, sen_len, w2v_path=w2v_path)
-embedding = preprocess.make_embedding(load=True)
-train_x = preprocess.sentence_word2idx()
-y = preprocess.labels_to_tensor(y)
+out_nolabel = []
+for i in range(3):
+    train_x_temp = train_x.copy()
+    y_temp = y.copy()
+    if i != 0 :
+        for j in range(len(out_nolabel)):
+            if out_nolabel[j] == 1:
+                train_x_temp.append(train_x_no_label[j])
+                y_temp.append(1)
+            elif out_nolabel[j] == 0:
+                train_x_temp.append(train_x_no_label[j])
+                y_temp.append(0)
+    # 對 input 跟 labels 做預處理
+    preprocess = Preprocess(train_x_temp, sen_len, w2v_path=w2v_path)
+    embedding = preprocess.make_embedding(load=True)
+    train_x_temp = preprocess.sentence_word2idx()
+    y_temp = preprocess.labels_to_tensor(y_temp)
 
-# 製作一個 model 的對象
-model = LSTM_Net(embedding, embedding_dim=300, hidden_dim=150, num_layers=2, dropout=0.5, fix_embedding=fix_embedding)
-model = model.to(device) # device為 "cuda"，model 使用 GPU 來訓練（餵進去的 inputs 也需要是 cuda tensor）
+    # 製作一個 model 的對象
+    #model = LSTM_Net(embedding, embedding_dim=300, hidden_dim=150, num_layers=2, dropout=0.5, fix_embedding=fix_embedding)
+    #model = model.to(device) # device為 "cuda"，model 使用 GPU 來訓練（餵進去的 inputs 也需要是 cuda tensor）
+    model = torch.load(os.path.join(model_dir, 'ckpt.model'))
+    model = model.to(device)
 
-# 把 data 分為 training data 跟 validation data（將一部份 training data 拿去當作 validation data）
-X_train, X_val, y_train, y_val = train_x[:180000], train_x[180000:], y[:180000], y[180000:]
-print(X_train[0])
+    # 把 data 分為 training data 跟 validation data（將一部份 training data 拿去當作 validation data）
+    X_val, X_train, y_val, y_train = train_x_temp[:20000], train_x_temp[180000:], y_temp[:20000], y_temp[180000:]
 
-# 把 data 做成 dataset 供 dataloader 取用
-train_dataset = TwitterDataset(X=X_train, y=y_train)
-val_dataset = TwitterDataset(X=X_val, y=y_val)
+    # 把 data 做成 dataset 供 dataloader 取用
+    train_dataset = TwitterDataset(X=X_train, y=y_train)
+    val_dataset = TwitterDataset(X=X_val, y=y_val)
 
-# 把 data 轉成 batch of tensors
-train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
-                                            batch_size = batch_size,
-                                            shuffle = True,
-                                            num_workers = 8)
+    # 把 data 轉成 batch of tensors
+    train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+                                                batch_size = batch_size,
+                                                shuffle = True,
+                                                num_workers = 8)
 
-val_loader = torch.utils.data.DataLoader(dataset = val_dataset,
+    val_loader = torch.utils.data.DataLoader(dataset = val_dataset,
+                                                batch_size = batch_size,
+                                                shuffle = False,
+                                                num_workers = 8)
+
+    # 開始訓練
+    training(batch_size, 5, lr, model_dir, train_loader, val_loader, model, device)
+
+    preprocess = Preprocess(train_x_no_label , sen_len, w2v_path=w2v_path)
+    embedding = preprocess.make_embedding(load=True)
+    test_x = preprocess.sentence_word2idx()
+    test_dataset = TwitterDataset(X=test_x, y=None)
+
+    test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
                                             batch_size = batch_size,
                                             shuffle = False,
                                             num_workers = 8)
+    model = torch.load(os.path.join(model_dir, 'ckpt.model'))
+    out_nolabel = psuedo_testing(batch_size, test_loader, model, device)
+    print(str(out_nolabel.count(1))+"\n")
+    print(str(out_nolabel.count(0))+"\n")
 
-# 開始訓練
-#training(batch_size, epoch, lr, model_dir, train_loader, val_loader, model, device)
+
+print('\nload model ...')
+model = torch.load(os.path.join(model_dir, 'ckpt.model'))
+outputs = testing(batch_size, test_loader, model, device)
 
 print("loading testing data ...")
 test_x = load_testing_data(testing_data)
